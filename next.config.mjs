810be4e6aec0d518a7da1bf6b77f51ec1e3306c0
sol-ninja24 <solninjaa27@gmail.com>
2026-07-10 22:51:12 +0000

    Ignore ESLint during Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  eslint: {
+      ignoreDuringBuilds: true,
+        },
+        };
+        
+        export default nextConfig;
